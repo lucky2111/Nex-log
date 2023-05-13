a3c7e05e4256cae02278952ea56e40bed29e0e28
lucky <121294325+lucky2111@users.noreply.github.com>
2023-05-13 08:06:02 +0300

    data base project new change

---
+++ src/nex/login/NexLogin.java
@@ -0,0 +1,13 @@
+
+package nex.login;
+
+public class NexLogin {
+
+   
+    public static void main(String[] args) {
+         login sign=new login();
+         sign .setVisible(true);
+         sign.setLocationRelativeTo(null);
+    }
+    
+}
